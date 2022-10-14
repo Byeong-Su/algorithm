a475f3f37251c903c0274fc7f560422d172481af
Byeong-Su <72130113+Byeong-Su@users.noreply.github.com>
2022-10-14 21:24:50 +0900

    .

--- Programmers/고득점 kit/정렬/가장 큰 수.cpp
+++ Programmers/고득점 kit/정렬/가장 큰 수.cpp
@@ -1,42 +1,28 @@
-#include <string>
 #include <vector>
+#include <string>
 #include <algorithm>
-#include <iostream>
-
 using namespace std;
-
-void dfs(int cnt, vector<string> v, bool visited[], vector<int> numbers, vector<int> &res){
-    if(cnt==numbers.size()){
-        string s;
-        for(int i=0; i<cnt; i++){
-            s+=v[i];
-        }
-        res.push_back(stoi(s));
-        return;
-    }
-    for(int i=0; i<numbers.size(); i++){
-        if(!visited[i]){
-            visited[i]=true;
-            v.push_back(to_string(numbers[i]));
-            dfs(cnt+1, v, visited, numbers, res);
-            v.pop_back();
-            visited[i]=false;
-        }
-    }
+ 
+//합한 문자 비교해 큰순으로 정렬
+// string1.compare(string2)가 아닌 <, >는 사전순으로 문자열을 정렬한다.
+bool compare(string a, string b) {
+    return a + b > b + a;
 }
-
+ 
 string solution(vector<int> numbers) {
     string answer = "";
-    
     vector<string> v;
-    vector<int> res;
-    bool visited[1001]={false};
-    
-    dfs(0, v, visited, numbers, res);
-    
-    sort(res.begin(), res.end(), greater<>());
-    
-    answer = to_string(res[0]);
-    
+    //int to string 한 후 벡터 v에 저장
+    for(int i=0; i<numbers.size(); i++){
+        v.push_back(to_string(numbers[i]));
+    }
+    //오름차순 정렬
+    sort(v.begin(), v.end(), compare);
+    //정렬된 문자열 합치기
+    for(int i=0; i<v.size(); i++){
+        answer += v[i];
+    }
+    //{0,0} 등 0만 있는경우 0이 아닌 00으로 이상하게 0을 출력하므로 예외처리
+    if(answer[0]=='0'){ return "0"; }
     return answer;
 }
