212a2a6de56d4e6c4aed595fd967d2820bec2e84
Byeong-Su <72130113+Byeong-Su@users.noreply.github.com>
2022-11-24 21:29:21 +0900

    Create 과일 장수.cpp

---
+++ Programmers/연습문제/과일 장수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer=0;
+    sort(score.begin(), score.end(), greater<>());
+    
+    int idx=m-1;
+    while(idx<score.size()){
+        answer+=(score[idx]*m);
+        idx+=m;
+    }   
+    
+    return answer;
+}
