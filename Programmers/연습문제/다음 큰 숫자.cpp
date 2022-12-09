e1d02155a8271e6ba96e5b0feeb47fe6faef4664
Byeong-Su <72130113+Byeong-Su@users.noreply.github.com>
2022-12-09 22:11:22 +0900

    .

--- Programmers/연습문제/다음 큰 숫자.cpp
+++ Programmers/연습문제/다음 큰 숫자.cpp
@@ -6,23 +6,20 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    //10진수 -> 2진수
-    stack<char> st;
-    int n_copy=n;
+    int next=n;
     //1의 개수 카운트용
     int n_cnt=0;
-    while(n_copy>0){
-        if(n_copy%2==1){ n_cnt++; }
-        n_copy/=2;
+    //10진수->2진수 변환해 1의개수 카운트
+    while(n>0){
+        if(n%2==1){ n_cnt++; }
+        n/=2;
     }
-    
-    int next=n;
     //다음 큰 수 찾기
     while(1){
         next++;
         //1의 개수 카운트용
         int next_cnt=0;
-        //next를 10진수->2진수
+        //next를 10진수->2진수 변환해 1의개수 카운트
         int next_copy=next;
         while(next_copy>0){
             if(next_copy%2==1){ next_cnt++; }
