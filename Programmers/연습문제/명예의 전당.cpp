adf53a42bfe6e57e9d9769d49980678894b1e451
Byeong-Su <72130113+Byeong-Su@users.noreply.github.com>
2022-12-18 20:31:57 +0900

    Create 명예의 전당.cpp

---
+++ Programmers/연습문제/명예의 전당.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    vector<int> honor;
+    for(int i=0; i<score.size(); i++){
+        honor.push_back(score[i]);
+        sort(honor.begin(), honor.end(), greater<>());
+        if(honor.size()<k){ answer.push_back(honor[i]); }
+        else { answer. push_back(honor[k-1]); }
+    }
+    return answer;
+}
